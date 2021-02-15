44af456fb4b2155f076fbab319e5e23716246df2
Eugene Hong <eugeneh1217@gmail.com>
2021-02-15 15:09:44 -0500

    hi

--- bot.py
+++ bot.py
@@ -3,7 +3,22 @@ import discord
 import datetime
 import time
 import asyncio
- 
+import json
+
+'''
+import json
+
+
+def write_to_json_file(data={}):
+    with open('linkdata.json') as json_file:
+        json_data = json_file.read()
+        json_data_formatted = json.loads(json_data)
+
+    with open('linkdata.json', 'w+') as json_file:
+        json_data_formatted.update(data)
+        json.dump(json_data_formatted, json_file)
+'''
+
 client = discord.Client()
 timeNow = datetime
 
@@ -21,8 +36,8 @@ Classtime = [
     []
 ]
 
-blocktimes = ["7:55", "9:00", "10:00", "11:00", "13:10", "13:55"]
-blockperioddata = [ 
+block_times = ["7:57", "8:57", "9:57", "10:57", "13:07", "13:57"]
+block_period_data = [ 
     [
         [1,2,3,4,5,6],
         [1,2,3,4,7,8],
@@ -39,51 +54,54 @@ blockperioddata = [
     ]
 ]
 
-ampmweek = 0
+am_pm_week = 0
 
 def block_to_period(block):
-    timeweekday = timeNow.datetime.now().strftime("%w")
+    time_week_day = timeNow.datetime.now().strftime("%w")
+
+    if(time_week_day == 5 or time_week_day == 6):
+        print("It's a weekend! F*** off!")
+        return 
+
     try:
-        return blockperioddata[ampmweek][int(timeweekday)][block-1]
+        return block_period_data[am_pm_week][int(time_week_day)][block-1]
     except:
-        print("\nThe day number is >>" + str(int(timeweekday)-1))
+        print("\nThe day number is >>" + str(int(time_week_day)-1))
         return
 
 async def send_interval_message():
     await client.wait_until_ready()
-    interval = 15
+    interval = 60
     while not client.is_closed():
 
-        timeFormated = timeNow.datetime.now().strftime("%H:%M")
+        timeFormatted = timeNow.datetime.now().strftime("%H:%M")
+        currentBlock = None
 
-        currentBlock = -1
-
-        for y in range(len(blocktimes)):
-
-            if blocktimes[y] == timeFormated:
+        for y in range(len(block_times)):
+            if block_times[y] == timeFormatted:
                 currentBlock = y + 1
                 print("The block has changed !!! >> " + str(currentBlock))
+                break
 
-        if currentBlock != -1:
+        if currentBlock:
 
             currentPeriod = block_to_period(currentBlock)
-            print("\nThe period has changed !!! >> " + str(currentPeriod) + "\n")
-            
-            for x in range(len(Classtime[currentPeriod-1])):
 
-                userid = Classtime[currentPeriod-1][x]
-                print("messaging this user >> " + str(userid))
+            if currentPeriod:
+                print("\nThe period has changed !!! >> " + str(currentPeriod) + "\n")
+                
+                for x in range(len(Classtime[currentPeriod-1])):
 
-                idperiod = userid.split("|")
+                    userid = Classtime[currentPeriod-1][x]
+                    print("messaging this user >> " + str(userid))
 
-                classLink = Linkdata[int(idperiod[0])][int(idperiod[1])-1]
-                userprofile = await client.fetch_user(int(idperiod[0]))
-                await userprofile.send("Join Your Class Here: " + classLink)
+                    idperiod = userid.split("|")
 
-                #channel = client.get_channel(800439342626897920)
-                #await channel.send(Linkdata[int(idperiod[0])][int(idperiod[1])])
-            
-            print("\n")
+                    classLink = Linkdata[int(idperiod[0])][int(idperiod[1])-1]
+                    userprofile = await client.fetch_user(int(idperiod[0]))
+                    await userprofile.send("Join Your Class Here: " + classLink)
+                
+                print("\n")
 
         await asyncio.sleep(interval)
 
@@ -95,41 +113,33 @@ async def on_ready():
 
 @client.event
 async def on_message(message):
-
     args = message.content.split(' ')
 
-    if(message.content.startswith('>setlink')):
+    if (args[0] == ">createprofile"):
+        Linkdata[message.author.id] = []
+        await message.reply("Created your profile!")
+
+    if(args[0] == '>setlink'):
 
         try:
-
             Linkdata[message.author.id].append(args[1])
 
         except:
-
-            print("\nCreating a new Profile... ", message.author)
-
-            Linkdata[message.author.id] = [[],[],[],[],[],[],[],[]]
-            Linkdata[message.author.id][int(args[2])-1] = args[1]
+            await message.reply("Do >createprofile first! :)")
+            return
 
         try:
-
-            Classtime[int(args[2])-1].append(str(message.author.id) + "|" + args[2])
+            Classtime[int(args[2])-1].append(str(message.author.id) + " | " + args[2])
 
         except:
             print("an error has happened")
-            #Classtime[args[2]]=[]
-            #Classtime[args[2]].append(str(message.author.id) + "|" + str(args[2]))
 
         print("All Link Information >> ", Linkdata)
         print("All Classtime Infromation >> ", Classtime, "\n")
 
         await message.reply("Added link for " + str(message.author))
 
-    if(message.content.startswith('>sendDM')):
-
-        await message.author.send('BRUH')
-
-    if(message.content.startswith('>deleteprofile')):
+    if (message.content.startswith('>deleteprofile')):
 
         del Linkdata[message.author.id]
         await message.reply(str(message.author.id) + "'s profile has been deleted")
@@ -141,5 +151,5 @@ async def on_message(message):
                 pass
 
 client.run("Nzk5NzYyMzUzMjU1NDgxMzg0.YAISuw.SXOXjdmOd5qyzVUVWeA1c6Z4En4")
- 
+
 
