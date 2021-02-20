0ed6f5ee0681215c693ec4401063bac009c4a5ad
Eugene Hong <eugeneh1217@gmail.com>
2021-02-19 22:07:19 -0500

    Did the easy fixes under code improvements

--- bot.py
+++ bot.py
@@ -20,8 +20,8 @@ def write_to_json_file(data={}):
 '''
 
 client = discord.Client()
-block_times = ["7:57", "8:57", "9:57", "10:57", "13:07", "13:52"]
-block_period_data = [ 
+BLOCK_TIMES = ["7:57", "8:57", "9:57", "10:57", "13:07", "13:52"]
+BLOCK_PERIOD_DATA = [ 
     [
         [1,2,3,4,5,6],
         [1,2,3,4,7,8],
@@ -38,9 +38,12 @@ block_period_data = [
     ]
 ]
 am_pm_week = 0
+ROLE_VOLUNTEER = 811698025600122942
+GUILD_ID = 799773078557163541
+LINKDATA_FILE = 'linkdata.json'
 
 def read_link_data():
-    with open('linkdata.json') as json_file:
+    with open(LINKDATA_FILE) as json_file:
         link_data_json = json_file.read()
         link_data = json.loads(link_data_json)
         print(link_data)
@@ -48,9 +51,8 @@ def read_link_data():
 Linkdata = read_link_data()
 
 def edit_link_data(Linkdata):
-    with open('linkdata.json', 'w+') as json_file:
+    with open(LINKDATA_FILE, 'w+') as json_file:
         json.dump(Linkdata, json_file)
-        return
 
 def block_to_period(block):
     time_week_day = datetime.datetime.now().strftime("%w")
@@ -58,25 +60,24 @@ def block_to_period(block):
         print("It's a weekend! F*** off!")
         return 
     try:
-        return block_period_data[am_pm_week][int(time_week_day)-1][block-1]
+        return BLOCK_PERIOD_DATA[am_pm_week][int(time_week_day)-1][block-1]
     except:
         print("\nThe day number is >>" + str(int(time_week_day)-1))
-        return
 
 async def check_for_role(role, userid):
-    server = client.get_guild(799773078557163541)
+    server = client.get_guild(GUILD_ID)
     user_member = await server.fetch_member(userid)
     for user_role in user_member.roles:
         if(user_role.id == role):
             return True
     return False
 
-async def createprofile(message):
+async def create_profile(message):
     Linkdata[str(message.author.id)] = ["" for i in range(8)]
     edit_link_data(Linkdata)
     await message.reply("Created your profile!")
 
-async def setLink(message):
+async def set_link(message):
     args = message.content.split(' ')
     try:
         Linkdata[str(message.author.id)][int(args[2])-1] = args[1]
@@ -85,14 +86,13 @@ async def setLink(message):
         if (not args[2]):
             await message.reply("Please provide a link and its corresponding period number!")
             return 
-        await message.reply("Do >createprofile first!")
-        return
+        await message.reply("Do >create_profile first!")
 
     print("All Link Information >> ", Linkdata)
 
     await message.reply("Added link for " + str(message.author))
 
-async def deleteProfile(message):
+async def delete_profile(message):
     del Linkdata[str(message.author.id)]
     edit_link_data(Linkdata)
     await message.reply(str(message.author.id) + "'s profile has been deleted.")
@@ -101,7 +101,7 @@ async def am_pm_week(message):
 
     args = message.content.split(' ')
 
-    if(await check_for_role(811698025600122942, message.author.id)):
+    if(await check_for_role(ROLE_VOLUNTEER, message.author.id)):
         if(args[1] == 'am'):
             am_pm_week = 0
         elif(args[1] == 'pm'):
@@ -114,8 +114,8 @@ async def send_interval_message():
     while not client.is_closed():
         timeFormatted = datetime.datetime.now().strftime("%H:%M")
         currentBlock = None
-        for y in range(len(block_times)):
-            if block_times[y] == timeFormatted:
+        for y in range(len(BLOCK_TIMES)):
+            if BLOCK_TIMES[y] == timeFormatted:
                 currentBlock = y + 1
                 print("The block has changed !!! >> " + str(currentBlock))
                 break
@@ -145,12 +145,12 @@ async def on_ready():
 @client.event
 async def on_message(message):
     args = message.content.split(' ')
-    if(args[0] == ">createprofile"):
-        await createprofile(message)
-    elif(args[0] == '>setlink'):
-        await setLink(message)
-    elif(args[0] == '>deleteprofile'):
-        await deleteProfile(message)
+    if(args[0] == ">create_profile"):
+        await create_profile(message)
+    elif(args[0] == '>set_link'):
+        await set_link(message)
+    elif(args[0] == '>delete_profile'):
+        await delete_profile(message)
     elif(args[0] == '>am_pm_week'):
         await am_pm_week(message)
     #if(args[0] == '>test'):
