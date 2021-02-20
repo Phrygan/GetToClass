2bd58529eb1a5bad63f29bb4a7038476e9dd1c06
Eugeneh1217 <Eugeneh1217@gmail.com>
2021-02-20 11:48:46 -0500

    created Command.py and util.py

---
+++ Command.py
@@ -0,0 +1,59 @@
+import asyncio
+import util
+
+class Command():
+    commands = []
+
+class Create_Profile(Command):
+    call = [">createprofile"]
+
+    @classmethod
+    async def run(cls, message, *args):
+        Linkdata[str(message.author.id)] = ["" for i in range(8)]
+        util.edit_link_data(util.Linkdata)
+        await message.reply("Created your profile!")
+
+class Set_Link(Command):
+    call = [">setlink"]
+
+    @classmethod
+    async def run(cls, message, *args):
+        args = message.content.split(' ')
+        try:
+            util.link_data[str(message.author.id)][int(args[2])-1] = args[1]
+            util.edit_link_data(util.link_data)
+        except:
+            if (not args[2]):
+                await message.reply("Please provide a link and its corresponding period number!")
+                return 
+            await message.reply("Do >create_profile first!")
+
+        print("All Link Information >> ", util.link_data)
+
+        await message.reply("Added link for " + str(message.author))
+
+class Delete_Profile(Command):
+    call = [">deleteprofile"]
+
+    @classmethod
+    async def run(cls, message, *args):
+        del util.link_data[str(message.author.id)]
+        util.edit_link_data(util.link_data)
+        await message.reply(str(message.author.id) + "'s profile has been deleted.")
+
+class Change_Am_Pm_week(Command):
+    call = [">changeampmweek"]
+
+    @classmethod
+    async def run(cls, message, *args):
+        command_args = message.content.split(' ')
+        if(await util.check_for_role(args[0], util.ROLE_VOLUNTEER, message.author.id)):
+            if(command_args[1] == 'am'):
+                am_pm_week = 0
+            elif(command_args[1] == 'pm'):
+                am_pm_week = 1
+            print("\nam_pm_week is now set to >> " + str(am_pm_week))
+
+Command.commands = Command.__subclasses__()
+
+    
---
+++ util.py
@@ -0,0 +1,56 @@
+import json
+import datetime
+
+ROLE_VOLUNTEER = 811698025600122942
+GUILD_ID = 799773078557163541
+LINKDATA_FILE = 'link_data.json'
+BLOCK_TIMES = ["7:57", "8:57", "9:57", "10:57", "13:07", "13:52"]
+BLOCK_PERIOD_DATA = [ 
+    [
+        [1,2,3,4,5,6],
+        [1,2,3,4,7,8],
+        [1,2,3,4], 
+        [5,6,7,8,1,2], 
+        [5,6,7,8,3,4]
+    ],
+    [
+        [1,2,3,4,5,6],
+        [1,2,3,4,7,8],
+        [5,6,7,8], 
+        [5,6,7,8,1,2], 
+        [5,6,7,8,3,4]
+    ]
+]
+
+am_pm_week = 0
+
+def read_link_data():
+    with open(LINKDATA_FILE) as json_file:
+        link_data_json = json_file.read()
+        link_data = json.loads(link_data_json)
+        print(link_data)
+        return link_data
+
+link_data = read_link_data()
+
+def edit_link_data(link_data):
+    with open(LINKDATA_FILE, 'w+') as json_file:
+        json.dump(link_data, json_file)
+
+def block_to_period(block):
+    time_week_day = datetime.datetime.now().strftime("%w")
+    if(time_week_day == 5 or time_week_day == 6):
+        print("It's a weekend! F*** off!")
+        return 
+    try:
+        return BLOCK_PERIOD_DATA[am_pm_week][int(time_week_day)-1][block-1]
+    except:
+        print("\nThe day number is >>" + str(int(time_week_day)-1))
+
+async def check_for_role(client, role, userid):
+    server = client.get_guild(GUILD_ID)
+    user_member = await server.fetch_member(userid)
+    for user_role in user_member.roles:
+        if(user_role.id == role):
+            return True
+    return False
