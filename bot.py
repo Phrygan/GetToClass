bafee6310282c45fc2075cb3d4f2597d1f5a9128
Eugeneh1217 <Eugeneh1217@gmail.com>
2021-02-21 13:45:27 -0500

    fixing more merge conflicts:

--- bot.py
+++ bot.py
@@ -38,8 +38,21 @@ async def send_interval_message():
 
                     dm_embed.add_field(name=f'Period {currentPeriod}', value=f'Link: {classLink}', inline=True)
                     await userprofile.send(embed=dm_embed)
-                await asyncio.sleep(interval)
         await asyncio.sleep(interval)
+
+def admin_terminal():
+    while not client.is_closed():
+        admin_in = input()
+        if ' ' in admin_in:
+            admin_in.split(' ')
+            admin_command_call = admin_in[0]
+            admin_command_args = admin_in[1:]
+        else:
+            admin_command_call = admin_in
+            admin_command_args = None
+        for admin_command in AdminCommand.AdminCommand.admin_commands:
+            if admin_command_call in admin_command.call:
+                admin_command.run(admin_command_args)
 
 @client.event
 async def on_ready():
